b5badeb0cf9922e412b4a373224dfc745329bd10
mylastresort <samytamim0@gmail.com>
2025-06-28 00:20:20 +0100

    chore: code refactoring & documenting the projection function

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,4 +1,5 @@
 use std::{
+    f32::consts::PI,
     fmt::Debug,
     ops::{Add, AddAssign, Index, IndexMut, Mul, MulAssign, Sub, SubAssign},
 };
@@ -460,20 +461,53 @@ impl<K: Scalar> Matrix<K> {
     }
 }
 
-pub fn projection(fov: f32, ratio: f32, near: f32, far: f32) -> Matrix<f32> {
-    let tan = f32::tan(fov.to_radians() / 2.);
-    let top = near * tan;
-    let right = top * ratio;
+pub fn projection(fov: f32, ratio: f32, n: f32, f: f32) -> Matrix<f32> {
+    // let Ps the projection of P on the image plane in 3D space.
+    // using the similar triangle rule:
+    //  (Ps)x = near * Px / -Pz
+    //  (Ps)y = near * Py / -Pz
+    // (-Pz is negative because camera is in opposite side)
+
+    // (Ps)w
+    // we start by deriving the last row of the matrix,
+    // to help us convert later the homogeneous point [x, y, z, w]
+    // to cartesian point [x/w, y/w, z/w]
+    // since camera is in the opposite side, (Ps)w = -Pz
+    // therefore, m32 -1
+
+    // (Ps)x
+    // left <= (Ps)x <= right
+    // we derive the value where the range is [-1, 1] in NDC space
+    // -1 <= 2 * n * Px / (-Pz * (r - l)) - (r + l) / (r - l) <= 1
+    // since (Ps)x will be divided at the end of the process by -Pz
+    // when we convert Ps from homogeneous to cartesian coordinates
+    // to have an identical formula we must assign these factors
+    // m00 (2*n/(r-l) and m02 ((r+l)/(r-l)) to the 1st and 3rd of (Ps)x
+
+    // (Ps)y
+    // similarly, we drive the value (Ps)y to be in [-1, 1] in NDC space
+    // we subtitute m11 (2 * n / (top - bottom)) and m12 ((top + right) / (top - bottom))
+
+    // (Ps)z
+    // since x and y of P don't depend on getting the z coordinate of Ps
+    // we are left with (Ps)z = (A * Pz + B * Pw) / -Pz (Pw is always 1)
+    // we derive A and B when (Ps)z is in range [0, 1]
+    // and subtitute a system of two eq -n*A+B=0 and -f*A+B=f
+    // we have A = m22 = -f/(f-n) and B = m23 = -(f*n)/(f-n)
+
+    // using trigonometry, we can express:
+    // tan(fov / 2) = opposite / adjacent = BC / AB = top / near
+    // top = tan(fov / 2) * near
+    // right = top * aspect_ratio
+    let tan = ((fov / 2.) * (PI / 180.)).tan();
+    let t = n * tan;
+    let r = t * ratio;
+    let (l, b) = (-r, -t);
 
     Matrix::from([
-        [near / right, 0.0, 0.0, 0.0],
-        [0.0, near / top, 0.0, 0.0],
-        [
-            0.0,
-            0.0,
-            -(far + near) / (far - near),
-            -2. * far * near / (far - near),
-        ],
-        [0.0, 0.0, -1.0, 0.0],
+        [2. * n / (r - l), 0., (r + l) / (r - l), 0.], // (Ps)x
+        [0., 2. * n / (t - b), (t + b) / (t - b), 0.], // (Ps)y
+        [0., 0., f / (n - f), (n * f) / (n - f)],      // (Ps)z
+        [0., 0., -1., 0.],                             // (Ps)w
     ])
 }
