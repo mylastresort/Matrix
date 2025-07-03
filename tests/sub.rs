665371b770f6266eb959012c403761b9b57a14fa
mylastresort <samytamim0@gmail.com>
2025-07-03 13:27:38 +0100

    refactor: refactor subtraction tests

--- tests/sub.rs
+++ tests/sub.rs
@@ -1,21 +1,62 @@
-use matrix::{Matrix, Vector};
+use matrix::{complex::Complex, Matrix, Vector, C, M, V};
 
 #[test]
-fn test_sub() {
-    let mut u = Vector::from([2., 3.]);
-    let v = Vector::from([5., 7.]);
+fn test_vector_sub() {
+    let mut u = V!([2., 3.]);
+    let v = V!([5., 7.]);
     u.sub(&v);
-    assert_eq!(u[0], -3.);
-    assert_eq!(u[1], -4.);
+    assert!(u.data.iter().eq(&[-3., -4.]));
+}
 
-    let mut u = Matrix::from([[1., 2.], [3., 4.]]);
-    let v = Matrix::from([[7., 4.], [-2., 2.]]);
+#[test]
+fn test_matrix_sub() {
+    let mut u = M!([[1., 2.], [3., 4.]]);
+    let v = M!([[7., 4.], [-2., 2.]]);
     u.sub(&v);
-    assert_eq!(u[0][0], -6.);
-    assert_eq!(u[0][1], -2.);
-    assert_eq!(u[1][0], 5.);
-    assert_eq!(u[1][1], 2.);
+    assert!(u.data.iter().eq(&[-6., -2., 5., 2.]));
+}
 
+#[test]
+fn test_vector_sub_2() {
+    for i in 0..20 {
+        let size = i;
+        let mut v = vec![];
+        for i in 0..size {
+            v.push(i as f32);
+        }
+        let mut a = V!(v);
+        let b = a.clone();
+        a.sub(&b);
+
+        for i in 0..size {
+            assert_eq!(a[i], 0.0, "a[i] must be zero after subtraction");
+        }
+    }
+}
+
+#[test]
+fn test_sub_complex_vector() {
+    let mut u = V!([C!(1., 2.), C!(-1., -3.)]);
+    let v = V!([C!(1., 3.), C!(2., 3.)]);
+    u.sub(&v);
+    assert!(u.data.iter().eq(&[C!(0., -1.), C!(-3., -6.)]));
+}
+
+#[test]
+fn test_sub_complex_matrix() {
+    let mut u = M!([[C!(1., 0.), C!(2., 0.)], [C!(3., 0.), C!(5., 0.)],]);
+    let v = M!([[C!(7., 0.), C!(3., 0.)], [C!(-2., 0.), C!(2., 0.)],]);
+    u.sub(&v);
+    assert!(u.data.iter().eq(&[
+        C!(-6., 0.),
+        C!(-1., 0.),
+        C!(5., 0.),
+        C!(3., 0.)
+    ]));
+}
+
+#[test]
+fn test_sub_complex() {
     for i in 0..20 {
         let size = i;
         let mut v = vec![];
@@ -27,7 +68,7 @@ fn test_sub() {
         a.sub(&b);
 
         for i in 0..size {
-            assert_eq!(a[i] + b[i], b[i], "a[i] and b[i] must equal")
+            assert_eq!(a[i], 0.0, "a[i] must be zero after subtraction");
         }
     }
 }
